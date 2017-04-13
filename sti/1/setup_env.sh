699019df62abbebb991a340360b1d4c52057b430
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2017-04-13 15:48:45 -0500

    setup_env.sh

---
+++ sti/1/setup_env.sh
@@ -0,0 +1,8 @@
+alias python='python3'
+TCL_PATH=/home/wozniak/Public/sfw/login/gcc/tcl-8.6.1-global/bin
+STC_PATH=/home/wozniak/Public/sfw/login/gcc/swift-t-py-3.6.1/stc/bin
+TURBINE_PATH=/home/wozniak/Public/sfw/login/gcc/swift-t-py-3.6.1/turbine/bin
+PY_PATH=/home/wozniak/Public/sfw/Python-3.6.1/bin
+export PATH=$TCL_PATH:$STC_PATH:$TURBINE_PATH:$PY_PATH:$PATH
+export  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/wozniak/Public/sfw/Python-3.6.1/lib
+export PYTHONPATH=/scratch/midway/yadunand/swift-e-lab/sti/1/libs/lib/python3.6/site-packages/:/scratch/midway/yadunand/swift-e-lab/sti/1/libs/lib/python3.6/::/scratch/midway/yadunand/swift-e-lab/sti/1/libs:/home/wozniak/Public/sfw/Python-3.6.1/lib:$PYTHONPATH
